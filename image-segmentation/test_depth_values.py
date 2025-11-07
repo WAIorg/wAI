3466ba3dd45bb867bdddcead0a4b8a6955ecf40f
Mackenzie Snyder <masnyder2@hotmail.com>
2025-11-07 13:03:03 -0500

    cean up folder from test images and add test depth values script

--- image-segmentation/test_depth_values.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-depth_img = np.load("./img_nov1/depth_raw_mm.npy")
-print("Depth shape:", depth_img.shape)
-
-sample_points = [(220, 40)]
-for x, y in sample_points:
-    print(f"Depth at ({x},{y}) = {depth_img[y, x]}")
